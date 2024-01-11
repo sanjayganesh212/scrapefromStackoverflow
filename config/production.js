9b47d9cb7dbb7d799f12bdfcd0975e8931873266
sanjayganesh212 <154579089+sanjayganesh212@users.noreply.github.com>
2024-01-11 21:43:26 +0530

    Add files via upload

---
+++ config/production.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+    PORT: 8080,
+
+    scrtky: "ce71510d995c53997c6ff12416a2300f",
+
+    fibwic: "34fdb5caa1abaec5",
+    
+    jwt : "e704aa5d48b4ae7c5cc0e7180c23e1b38effdd38faebf7b4f9",
+
+    db_prefix : "STK",
+
+    mongodbConnectivityString: "U2FsdGVkX19mrVceHkeYijih6CiVyzxqItBwszzCu8K6rMuTtG9ait9gOqnxcbClaUDpFSYGIIuoQ9VwYCqv/g=="
+
+}
